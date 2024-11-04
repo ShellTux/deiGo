56f311286919340b1ace969b98690bfd4c59698c
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-11-04 11:46:52 +0000

    chore: update ast.c and ast.h

--- include/ast.h
+++ include/ast.h
@@ -1,39 +1,65 @@
+/***************************************************************************
+ * Project                                           _      _  ____
+ *                                                __| | ___(_)/ ___| ___
+ *                                               / _` |/ _ \ | |  _ / _ \
+ *                                              | (_| |  __/ | |_| | (_) |
+ *                                               \__,_|\___|_|\____|\___/
+ *
+ *
+ *
+ * Authors: João Fonseca, Luís Góis
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
 #ifndef _AST_H
 #define _AST_H
 
-enum category {
-  Program,
-  Function,
-  Parameters,
-  Parameter,
-  Arguments,
-  Integer,
-  Double,
-  Identifier,
-  Natural,
-  Decimal,
-  Call,
-  If,
-  Add,
-  Sub,
-  Mul,
-  Div
-};
+#define CATEGORIES                                                             \
+  CATEGORY(Program)                                                            \
+  CATEGORY(Function)                                                           \
+  CATEGORY(Parameters)                                                         \
+  CATEGORY(Parameter)                                                          \
+  CATEGORY(Arguments)                                                          \
+  CATEGORY(Integer)                                                            \
+  CATEGORY(Double)                                                             \
+  CATEGORY(Identifier)                                                         \
+  CATEGORY(Natural)                                                            \
+  CATEGORY(Decimal)                                                            \
+  CATEGORY(Call)                                                               \
+  CATEGORY(If)                                                                 \
+  CATEGORY(Add)                                                                \
+  CATEGORY(Sub)                                                                \
+  CATEGORY(Mul)                                                                \
+  CATEGORY(Div)
 
-struct node {
-  enum category category;
+typedef enum category {
+#define CATEGORY(ENUM) ENUM,
+  CATEGORIES
+#undef CATEGORY
+} Category;
+
+void printCategory(const Category category);
+
+typedef struct TreeNode {
   char *token;
-  struct node_list *children;
-};
+  char *id;
+  struct TreeNode *newBranch;
+  struct TreeNode *currentBranch;
+} TreeNode;
 
-struct node_list {
-  struct node *node;
-  struct node_list *next;
-};
-
-struct node *newnode(enum category category, char *token);
-void addchild(struct node *parent, struct node *child);
-void print(enum category category, char *token, int depth);
-void show(struct node *node, int depth);
+TreeNode *newNode(char *id, char *token, TreeNode *newBranch);
+void linkInLevel(TreeNode *node, TreeNode *nodeLevel);
+void linkChild(TreeNode *parent, TreeNode *newBranch);
+void shiftInsertNode(TreeNode *parent, TreeNode *nodeNew);
+void show(TreeNode *node, int depth);
 
 #endif
--- src/ast.c
+++ src/ast.c
@@ -1,35 +1,114 @@
+/***************************************************************************
+ * Project                                           _      _  ____
+ *                                                __| | ___(_)/ ___| ___
+ *                                               / _` |/ _ \ | |  _ / _ \
+ *                                              | (_| |  __/ | |_| | (_) |
+ *                                               \__,_|\___|_|\____|\___/
+ *
+ *
+ *
+ * Authors: João Fonseca, Luís Góis
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
 #include "ast.h"
+
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node *newnode(enum category category, char *token) {
-  struct node *new = malloc(sizeof(struct node));
-  new->category = category;
-  new->token = token;
-  new->children = malloc(sizeof(struct node_list));
-  new->children->node = NULL;
-  new->children->next = NULL;
+TreeNode *newNode(char *id, char *token, TreeNode *newBranch) {
+  TreeNode *new = malloc(sizeof(TreeNode));
+  if (token == NULL) {
+    return NULL;
+  }
+
+  *new = (TreeNode){
+      .token = token,
+      .id = id,
+      .currentBranch = NULL,
+      .newBranch = newBranch,
+  };
+
   return new;
 }
 
-void addchild(struct node *parent, struct node *child) {
-  struct node_list *new = malloc(sizeof(struct node_list));
-  new->node = child;
-  new->next = NULL;
-  struct node_list *children = parent->children;
-  while (children->next != NULL) {
-    children = children->next;
+void linkInLevel(TreeNode *node, TreeNode *nodeLevel) {
+  if (node == NULL) {
+    node = nodeLevel;
+    return;
   }
-  children->next = new;
+
+  if (nodeLevel == NULL) {
+    return;
+  }
+
+  TreeNode *tmp = NULL;
+  for (tmp = node; tmp->currentBranch != NULL; tmp = tmp->currentBranch) {
+  }
+  assert(tmp != NULL);
+  tmp->currentBranch = nodeLevel;
 }
 
-void print(enum category category, char *token, int depth) {
-  (void)category;
-  (void)token;
-  (void)depth;
+void linkChild(TreeNode *parent, TreeNode *newBranch) {
+  if (parent == NULL || newBranch == NULL) {
+    return;
+  }
+
+  if (parent->newBranch == NULL) {
+    parent->newBranch = newBranch;
+  } else {
+    linkInLevel(parent->newBranch, newBranch);
+  }
 }
 
-void show(struct node *node, int depth) {
-  (void)node;
-  (void)depth;
+void shiftInsertNode(TreeNode *parent, TreeNode *nodeNew) {
+  if (parent == NULL || nodeNew == NULL) {
+    return;
+  }
+
+  TreeNode *nodeTemp = NULL;
+  if (parent->newBranch == NULL) {
+    parent->newBranch = nodeNew;
+  } else {
+    nodeTemp = parent->newBranch;
+    parent->newBranch = nodeNew;
+    parent->newBranch->currentBranch = nodeTemp;
+  }
+
+  TreeNode *auxNode = NULL;
+  for (auxNode = parent->currentBranch; auxNode != NULL;
+       auxNode = auxNode->currentBranch) {
+    nodeTemp = auxNode->newBranch;
+    auxNode->newBranch = newNode(NULL, nodeNew->token, NULL);
+    auxNode->newBranch->currentBranch = nodeTemp;
+  }
+}
+
+void show(TreeNode *node, int depth) {
+  if (node == NULL) {
+    return;
+  }
+
+  for (int i = 0; i < depth; i++) {
+    printf("..");
+  }
+
+  if (node->id != NULL) {
+    printf("%s(%s)\n", node->token, node->id);
+  } else {
+    printf("%s\n", node->token);
+  }
+
+  show(node->newBranch, depth + 1);
+  show(node->currentBranch, depth);
 }
