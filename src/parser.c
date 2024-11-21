0c0224ac880e3de61bb0d6b87be02d8c5b598d4f
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-11-21 15:16:00 +0000

    chore: add header with license and authors

--- src/parser.c
+++ src/parser.c
@@ -1,3 +1,27 @@
+/* João Fonseca 2021227194 */
+/* Luís Pedro de Sousa Oliveira Góis 2018280716 */
+/***************************************************************************
+ * Project                                           _      _  ____
+ *                                                __| | ___(_)/ ___| ___
+ *                                               / _` |/ _ \ | |  _ / _ \
+ *                                              | (_| |  __/ | |_| | (_) |
+ *                                               \__,_|\___|_|\____|\___/
+ *
+ *
+ *
+ * Authors: João Fonseca, Luís Góis
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
 #include "parser.h"
 #include "stdbool.h"
 
